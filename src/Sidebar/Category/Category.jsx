efbd8e0a90f53bdaaa1d785c379b13d8752101bb
Satyapal <satyapaltiwari920@gmail.com>
2026-01-30 23:51:34 +0530

    project stopped

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -3,7 +3,27 @@ import "./Category.css"
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <div>
+      <h2 className="sidebar-title">Category</h2>
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark">All</span>
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark">Flats</span>
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark">Sandals</span>
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name='test' />
+          <span className="checkmark">Heels</span>
+        </label>
+      </div>
+    </div>
   )
 }
 
